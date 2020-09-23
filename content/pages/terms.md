42b46e435a646aaec34a8764cf1393ed90438342
krissanawat <krissnawat101@gmail.com>
2020-09-23 12:52:47 +0700

    init

---
+++ content/pages/terms.md
@@ -0,0 +1,37 @@
+---
+template: "page"
+title: "Terms of use"
+description: "This is a description on Terms of use"
+slug: "terms"
+---
+
+<p>kriss Terms of Service</p>
+<p>1. Terms</p>
+<p>By accessing the website at https://flutterdaily.dev, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+<p>2. Use License</p>
+<p>Permission is granted to temporarily download one copy of the materials (information or software) on kriss' website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+<p></p>
+<p>modify or copy the materials;</p>
+<p>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</p>
+<p>attempt to decompile or reverse engineer any software contained on kriss' website;</p>
+<p>remove any copyright or other proprietary notations from the materials; or</p>
+<p>transfer the materials to another person or "mirror" the materials on any other server.</p>
+<p></p>
+<p></p>
+<p>This license shall automatically terminate if you violate any of these restrictions and may be terminated by kriss at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
+<p></p>
+<p>3. Disclaimer</p>
+<p>The materials on kriss' website are provided on an 'as is' basis. kriss makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+<p>Further, kriss does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+<p></p>
+<p>4. Limitations</p>
+<p>In no event shall kriss or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on kriss' website, even if kriss or a kriss authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+<p>5. Accuracy of materials</p>
+<p>The materials appearing on kriss' website could include technical, typographical, or photographic errors. kriss does not warrant that any of the materials on its website are accurate, complete or current. kriss may make changes to the materials contained on its website at any time without notice. However kriss does not make any commitment to update the materials.</p>
+<p>6. Links</p>
+<p>kriss has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by kriss of the site. Use of any such linked website is at the user's own risk.</p>
+<p>7. Modifications</p>
+<p>kriss may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+<p>8. Governing Law</p>
+<p>These terms and conditions are governed by and construed in accordance with the laws of Thailand and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+<p>Terms of Use created with GetTerms.</p>
---
+++ data/SiteConfig.js
@@ -0,0 +1,155 @@
+const config = {
+  // Site info
+  siteTitle: "Flutter Daily - daily tips and trick for Flutterien", // Site title.
+  siteTitleShort: "flutterdaily.dev", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "Flutter Daily - daily tips and trick for Flutterien", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024x1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://flutterdaily.dev", // Domain of your website without pathPrefix.
+  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "Flutter Daily - daily tips and trick for Flutterien", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteLang: "en",
+  siteFBAppID: "2198345126924491", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-137554402-1", // GA tracking ID.
+  postDefaultCategoryID: "", // Default category for posts.
+  // Common for tag, category pages and widget
+  numberLatestPost: 8,
+  postsPerPage: 6,
+  // Use for post
+  dateFromFormat: "YYYY-MM-DDTHH:mm:ssZ", // Date format used in the frontmatter.
+  dateFormat: "MMMM Do, YYYY", // Date format for display.
+  postTagged: "",
+  postInCategories: "",
+  postOnDate: "Posted on",
+  // Use for comment
+  lazyLoadComments: true,
+  disqusShortname: "krissio", // Disqus shortname.
+  btnLoadComments: "Load comments",
+  // Use for home page
+  numberLoadmore: 6,
+  btnLoadmore: "Load more",
+  homeHasLoadmore: true,
+  homeHasThumbnail: true,
+  homeHeader: "Home",
+  // Use for page
+  pathPrefixPagination: "/page", // Prefix path for pagination
+  pageNotFoundTitle: "Page Not Found", //
+  pageNotFoundBtn: "Back to our site",
+  pageNotFoundContent:
+    "Looks like you've followed a broken link or entered a URL that doesn't exist on this site.",
+  // Use for tag
+  pathPrefixTag: "/tag", // Prefix path for tags
+  tagHeader: "Posts tagged as", // use in header of tag-template page
+  tagHasThumbnail: true,
+  // Use for category
+  pathPrefixCategory: "/category", // Prefix path for category
+  categoryHeader: "Posts in category", // use in header of category-template page
+  categoryHasThumbnail: true,
+  // Use for widget
+  categoryWidgetTitle: "Categories",
+  tagWidgetTitle: "Tags",
+  latestPostsWidgetTitle: "Latest posts",
+  linksWidgetTitle: "Links",
+  // Use for Google custom search
+  searchWidgetTitle: "What your looking for?",
+  searchWidgetPlaceHolder: "Enter keyword",
+  searchEngineID: "005328897267550362033:gui3_qqia8k",
+  hasSearch: true,
+  // Use for links widget
+  sidebarSticky: true,
+
+  // Use for user info
+  userName: "Krissanawat", // Username to display in the author segment.
+  userEmail: "wow@kriss.io", // Email used for RSS feed"s author segment
+  userTwitter: "flutter_daily", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Chiangmai, Thailand", // User location to display in the author segment.
+  userAvatar:
+    "https://en.gravatar.com/userimage/155811895/09834b18c86688d77a606ab08b38cec3.png?s=300", // User avatar to display in the author segment.
+  userDescription:
+    "Flutter Develiper @instaflutter.com, Dart Lover & Blogger @flutter_daily.", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "Email",
+      url: "mailto:wow@flutterdaily.dev",
+      iconClassName: "far envelope",
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/flutter_daily",
+      iconClassName: "fab twitter",
+    },
+    {
+      label: "Facebook",
+      url: "https://www.facebook.com/groups/614234665932028",
+      iconClassName: "fab facebook-f",
+    },
+  ],
+  // Use for navigation
+  navTitle: "Flutter Daily",
+  navLinks: [
+    { label: "About", url: "/about" },
+    { label: "Contact", url: "/contact" },
+  ],
+  // Use for footer
+  socialLinks: [
+    {
+      label: "FreeCodeCamp",
+      url: "", // Ignore
+      iconClassName: "fab free-code-camp",
+    },
+    {
+      label: "GitHub",
+      url: "https://github.com/krisnawat",
+      iconClassName: "fab github",
+    },
+    {
+      label: "Facebook",
+      url: "https://www.facebook.com/flutterdaily.dev/",
+      iconClassName: "fab facebook-f",
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/flutter_daily",
+      iconClassName: "fab twitter",
+    },
+    {
+      label: "RSS",
+      url: "https://kriss.io/rss.xml",
+      iconClassName: "fas rss",
+    },
+  ],
+  footerLinks: [
+    { label: "Home", url: "/" },
+    { label: "About", url: "/about" },
+    { label: "Contact", url: "/contact" },
+    { label: "Terms of Use", url: "/terms" },
+    { label: "Privacy Policy", url: "/privacy" },
+    { label: "Sitemap", url: "https://flutterdaily.dev/sitemap.xml" },
+  ],
+  copyright:
+    "Copyright © 2019-2020 Chiangmai. Unless otherwise noted, all code MIT license.",
+  // Use for manifest
+  themeColor: "#2196F3", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#FFF", // Used for setting manifest background color.
+};
+
+// Validate
+
+// Make sure pathPrefix is empty if not needed
+if (config.pathPrefix === "/" || config.pathPrefix === "") {
+  config.pathPrefix = "";
+} else {
+  // Make sure pathPrefix only contains the first forward slash
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
+}
+
+// Make sure siteUrl doesn't have an ending forward slash
+if (config.siteUrl.substr(-1) === "/")
+  config.siteUrl = config.siteUrl.slice(0, -1);
+
+// Make sure siteRss has a starting forward slash
+if (config.siteRss && config.siteRss[0] !== "/")
+  config.siteRss = `/${config.siteRss}`;
+
+module.exports = config;
